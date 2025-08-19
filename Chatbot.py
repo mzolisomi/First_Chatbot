3deb6e708c8544e7858976c752491bfa775101b4
Mzoli <167242849+mzolisomi@users.noreply.github.com>
2025-08-20 01:54:51 +0200

    2rd changes

--- Chatbot.py
+++ Chatbot.py
@@ -1,1 +1,18 @@
-print("Hello Chatbot")
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Create chatbot
+chatbot = ChatBot("SupportBot")
+
+# Train chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("./FAQ's.yml")
+
+# Chat loop
+print("Hi User")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "quit":
+        break
+    response = chatbot.get_response(user_input)
+    print("Bot:", response)
